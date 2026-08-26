00156893f597c9759078fa42ea12b7a947710d42
Doooly <jun2133889@gmail.com>
2026-08-26 16:19:25 +0900

    Update server-config.js

--- server-config.js
+++ server-config.js
@@ -1,4 +1,4 @@
 // 현장처리 서버 주소 설정
 // 테스트용 Cloudflare Quick Tunnel URL을 이 한 곳에서 관리합니다.
 // start-all.bat 실행 후 표시되는 https://xxxx.trycloudflare.com 주소로 교체하세요.
-window.FIELD_SERVER_URL = 'https://actress-numbers-heights-rates.trycloudflare.com';
+window.FIELD_SERVER_URL = 'https://triple-internal-work-securities.trycloudflare.com';
