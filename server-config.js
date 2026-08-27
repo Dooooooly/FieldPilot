9636d081b06095a3fea4111769310b9ac389a991
Doooly <jun2133889@gmail.com>
2026-08-27 15:02:27 +0900

    Add files via upload

--- server-config.js
+++ server-config.js
@@ -1,3 +1,3 @@
 ﻿// ?꾩옣泥섎━ ?쒕쾭 二쇱냼 ?ㅼ젙
 // Cloudflare Quick Tunnel URL - ?뚯뒪?몄슜
-window.FIELD_SERVER_URL = 'https://cuisine-consultants-merit-third.trycloudflare.com';
+window.FIELD_SERVER_URL = 'https://urban-feeling-dining-seeker.trycloudflare.com';
