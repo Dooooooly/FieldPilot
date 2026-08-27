29d4dcad767390a7868be96342525a9f89428c9e
Doooly <jun2133889@gmail.com>
2026-08-27 18:05:13 +0900

    Update server-config.js

--- server-config.js
+++ server-config.js
@@ -2,4 +2,4 @@
 // 노트북 서버(server.js)가 실행 중인 주소를 넣으세요.
 // - 같은 와이파이: http://<노트북 내부IP>:3000  (예: http://192.168.1.100:3000)
 // - Cloudflare Quick Tunnel 사용 시: https://xxxx.trycloudflare.com (터널 재시작마다 바뀜)
-window.FIELD_SERVER_URL = 'https://subcommittee-reasons-programme-library.trycloudflare.com';
+window.FIELD_SERVER_URL = 'https://implemented-wagon-destination-scope.trycloudflare.com';
