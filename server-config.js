38c7d965a257840ac973ead4e4b775b22b009661
Doooly <jun2133889@gmail.com>
2026-08-28 13:57:55 +0900

    ?? ??: Cloudflare Tunnel ?? ?? (2026-08-28 13:57)

--- server-config.js
+++ server-config.js
@@ -1,2 +1,2 @@
 // 현장처리 서버 주소 설정 (Cloudflare Quick Tunnel - 재시작마다 바뀝니다)
-window.FIELD_SERVER_URL = 'https://hampton-cocktail-regional-duration.trycloudflare.com';
+window.FIELD_SERVER_URL = 'https://holmes-optics-stating-steering.trycloudflare.com';
