5beec3635165e5467af53ccda881241c4d2371d0
Doooly <jun2133889@gmail.com>
2026-09-02 17:30:19 +0900

    ?? ??: Cloudflare Tunnel ?? ?? (2026-09-02 17:30)

--- server-config.js
+++ server-config.js
@@ -1,2 +1,2 @@
 // 현장처리 서버 주소 설정 (Cloudflare Quick Tunnel - 재시작마다 바뀝니다)
-window.FIELD_SERVER_URL = 'https://regardless-heads-pie-systematic.trycloudflare.com';
+window.FIELD_SERVER_URL = 'https://ignored-heroes-front-navigate.trycloudflare.com';
