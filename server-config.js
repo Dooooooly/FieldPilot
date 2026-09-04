c4fb47aad543b245140cba0afa792eea75be8b3c
Doooly <jun2133889@gmail.com>
2026-09-04 15:55:42 +0900

    ?? ??: Cloudflare Tunnel ?? ?? (2026-09-04 15:55)

--- server-config.js
+++ server-config.js
@@ -1,2 +1,2 @@
 // 현장처리 서버 주소 설정 (Cloudflare Quick Tunnel - 재시작마다 바뀝니다)
-window.FIELD_SERVER_URL = 'https://highway-verbal-reserves-conventions.trycloudflare.com';
+window.FIELD_SERVER_URL = 'https://allocation-investments-sequences-cab.trycloudflare.com';
